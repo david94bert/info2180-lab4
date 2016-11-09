b474fa0cf037fa97a5c32f508a0e922ef0644a35
david94bert <david94bert@yahoo.com>
2016-11-09 17:33:21 -0500

    question 1
    after struggling question one is complete

--- maze.js
+++ maze.js
@@ -1,1 +1,15 @@
-understanding
+
+window.onload = function(){
+
+   document.getElementById("boundary1").innerHTML="helo";
+   go();
+   
+
+}
+function go(){
+    document.getElementById("boundary1").addEventListener("mouseover",change);
+    function change(){
+        document.getElementById("boundary1").className="boundary youlose";
+    }
+
+}
