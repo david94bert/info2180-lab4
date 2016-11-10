0bc11aac6f893fff71100388a12dd2e82954c3a2
david94bert <david94bert@yahoo.com>
2016-11-09 20:57:08 -0500

    question3

--- maze.js
+++ maze.js
@@ -1,29 +1,60 @@
-
 //var bnd = document.querySelectorAll(".boundary");
+
+var lost = "false";
 window.onload = function(){
 
    
    var yo= document.getElementById("boundary1");
-   go(yo);
+   
+   //console.log(lost);
+  
+  
    goAll();
+    
+  
+   ended(lost);
+  // console.log(lost);
 
 }
 
+function gameSet(){
+    
+}
 
 function go(g){
     g.addEventListener("mouseover",change);
     function change(){
+        lost="true";
+         //console.log(lost);
         var bnd = document.querySelectorAll(".boundary");
-        for(var i in bnd){
+        for(var i = 0; i < bnd.length-1; i++){
        bnd[i].className="boundary youlose";
+       
     }
         
     }
 
-}
+};
 function goAll(){
      var bnd = document.querySelectorAll(".boundary");
-    for(var i in bnd){
+    // console.log(bnd);
+    for(var i = 0; i < bnd.length-1; i++){
        go(bnd[i])
     }
-}
+};
+
+
+function ended(l){
+    document.getElementById("end").addEventListener("mouseover",winOrLose);
+    function winOrLose(){
+        //console.log(l);
+    if(document.getElementById("boundary1").className==="boundary youlose"){
+        document.getElementById("status").innerHTML="You Lose!"
+    }
+    else{
+        document.getElementById("status").innerHTML = "You Win!"
+        
+
+    }
+};
+};
